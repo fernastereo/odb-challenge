f73d35fdb9137984da6ecd6251480d0a94d3550e
Fernando Cueto <fernandoecueto@gmail.com>
2023-03-20 00:40:09 +0100

    adds plugin to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -41,6 +42,9 @@ module.exports = {
       inject: true,
       template: "./public/index.html",
       filename: "./index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "./public/styles.css", to: "" }],
     }),
   ],
 };
