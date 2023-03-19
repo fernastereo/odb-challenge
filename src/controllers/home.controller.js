88970b8a6c533c64ca1f23cd961b670d6e7137b7
Fernando Cueto <fernandoecueto@gmail.com>
2023-03-19 23:42:58 +0100

    adds spinner animation

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,6 +1,7 @@
 import view from "../pages/home.html";
 import prepareData from "../utils/prepareData";
 import Repository from "../templates/Repository";
+import Spinner from "../templates/Spinner";
 
 const Home = () => {
   const divElement = document.createElement("div");
@@ -9,10 +10,14 @@ const Home = () => {
   const btnSearchRepo = divElement.querySelector("#btnSearchRepo");
   const txtSearchRepo = divElement.querySelector("#txtSearchRepo");
   btnSearchRepo.addEventListener("click", async () => {
+    const loading = divElement.querySelector("#loading");
+    loading.innerHTML = await Spinner();
+
     const resp = await prepareData(txtSearchRepo.value);
 
     const divResponse = divElement.querySelector("#response");
     divResponse.innerHTML = Repository(resp);
+    loading.innerHTML = "";
   });
 
   return divElement;
