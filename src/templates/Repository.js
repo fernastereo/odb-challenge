c531163c25a6f492171bd2ac75fc72fec3251c39
Fernando Cueto <fernandoecueto@gmail.com>
2023-03-19 23:24:46 +0100

    deletes hardcoded data

--- src/templates/Repository.js
+++ src/templates/Repository.js
@@ -53,7 +53,7 @@ const Repository = (repositories) => {
                         <span class="font-medium text-gray-900 flex">${
                           repository.stargazers_count * 1 +
                             repository.forks_count * 2 >=
-                          1
+                          500
                             ? `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 fill-yellow-500 text-gray-800">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z" />
                         </svg> <span>Popular</span>`
